270652904ea9e34484fc6455c4d9ecdb24c0654a
+ <jacqueline.trapp18@gmail.com>
2024-12-10 14:27:55 -0500

    adding back cors for json-server methodology

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const jsonServer = require('json-server');
+const cors = require('cors');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 // Use the environment variable PORT or default to 8080
 const port = process.env.PORT || 3000;
+
+const allowedOrigins = ['http://localhost:8080', 'http://localhost:3000', 'https://jtrapp18.github.io/english-study-curriculum-mgmt'];
+
+server.use(cors({
+  origin: allowedOrigins, // Only allow these origins
+}));
 
 server.use(middlewares);
 server.use(router);
